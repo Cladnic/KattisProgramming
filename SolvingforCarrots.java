84b0c436256d5f1a32f8952fe3b33ea50fb07d00
Cladnic <cladnic@hotmail.com>
2021-04-22 21:58:55 +0200

    Create SolvingforCarrots.java

---
+++ SolvingforCarrots.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scannerInput = new Scanner(System.in);
+        int carrots = scannerInput.nextInt();
+        carrots = scannerInput.nextInt();
+
+        System.out.println(carrots);
+    }
+}
