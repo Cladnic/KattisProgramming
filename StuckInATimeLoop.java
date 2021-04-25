6e13618e7dfb4c679de80fd84d291805dc5c1538
Cladnic <cladnic@hotmail.com>
2021-04-26 00:47:46 +0200

    Create StuckInATimeLoop.java

---
+++ StuckInATimeLoop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scannerInput = new Scanner(System.in);
+        int amount = scannerInput.nextInt();
+
+        for(int i=0; i<amount; i++){
+            System.out.println(i+1 + " Abracadabra");
+        }
+    }
+}
